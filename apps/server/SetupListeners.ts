e2dec1c307c278ffaa554678f47ca0e098dec69e
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2024-08-05 16:20:32 +0530

    fix: typo

--- apps/server/SetupListeners.ts
+++ apps/server/SetupListeners.ts
@@ -6,40 +6,33 @@ export function setupListeners(io: Server) {
   io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
-    socket.on(
-      "join-game",
-      (roomId: string, name: string, gameType: "friendly" | "paid") => {
-        if (!roomId) {
-          return socket.emit("error", "Room ID is required");
-        }
-
-        if (!name) {
-          return socket.emit("error", "Name is required");
-        }
-
-        if (!gameType) {
-          return socket.emit("error", "Game type is required");
-        }
-
-        socket.join(roomId);
-
-        if (rooms.has(roomId)) {
-          const game = rooms.get(roomId);
-
-          if (!game) {
-            return socket.emit("error", "Game not found");
-          }
-
-          game.joinPlayer(socket.id, name, socket);
-        } else {
-          const game = new Game(roomId, io, socket.id);
-          game.joinPlayer(socket.id, name, socket);
-          rooms.set(roomId, game);
-        }
-
-        io.to(roomId).emit("user-joined", { name, gameType });
+    socket.on("join-game", (roomId: string, name: string) => {
+      if (!roomId) {
+        return socket.emit("error", "Room ID is required");
       }
-    );
+
+      if (!name) {
+        return socket.emit("error", "Name is required");
+      }
+
+      socket.join(roomId);
+
+      if (rooms.has(roomId)) {
+        const game = rooms.get(roomId);
+
+        if (!game) {
+          return socket.emit("error", "Game not found");
+        }
+
+        game.joinPlayer(socket.id, name, socket);
+      } else {
+        const game = new Game(roomId, io, socket.id);
+        game.joinPlayer(socket.id, name, socket);
+        rooms.set(roomId, game);
+      }
+
+      io.to(roomId).emit("user-joined", { name });
+    });
 
     socket.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
